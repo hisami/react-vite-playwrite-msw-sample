2be6e70b508f3ee3fe70e66868943219a7f3f646
hisamitsu <koron.take@gmail.com>
2023-01-23 17:34:06 +0900

    playwrightの設定ファイル作成

---
+++ playwright.config.ts
@@ -0,0 +1,25 @@
+import { devices, PlaywrightTestConfig } from '@playwright/test'
+
+const config: PlaywrightTestConfig = {
+  fullyParallel: true,
+
+  // マルチブラウザテストを行う
+  projects: [
+    {
+      name: 'Pixel 4',
+      use: {
+        browserName: 'chromium',
+        ...devices['Pixel 4']
+      }
+    },
+    {
+      name: 'iPhone 11',
+      use: {
+        browserName: 'webkit',
+        ...devices['iPhone 11']
+      }
+    }
+  ]
+}
+
+export default config
