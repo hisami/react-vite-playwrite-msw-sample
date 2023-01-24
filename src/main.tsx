42fe373af385117de1ddde2ae2edcfb2acef4011
hisami <ryohei.hisamitsu@ncdc.co.jp>
2023-01-24 09:50:52 +0900

    MSWの設定

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { RegisterUser } from './register-user'
-import { MyPage } from './my-page'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { RegisterUser } from "./register-user";
+import { MyPage } from "./my-page";
 
+(async () => {
+  if (import.meta.env.VITE_MOCKED_API === "true") {
+    const { buildMswWorker } = await import("./testing-util/msw-util");
+    const worker = buildMswWorker();
+    await worker.start({
+      onUnhandledRequest: "bypass",
+    });
+  }
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/register-user" element={<RegisterUser />} />
-        <Route path="/my-page" element={<MyPage />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>,
-)
+  ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/register-user" element={<RegisterUser />} />
+          <Route path="/my-page" element={<MyPage />} />
+        </Routes>
+      </BrowserRouter>
+    </React.StrictMode>
+  );
+})();
--- src/testing-util/msw-util.ts
+++ src/testing-util/msw-util.ts
@@ -1,13 +1,13 @@
-import { setupWorker, rest, SetupWorkerApi } from 'msw'
+import { setupWorker, rest, SetupWorkerApi } from "msw";
 
-export const worker = (): SetupWorkerApi => {
+export const buildMswWorker = (): SetupWorkerApi => {
   return setupWorker(
-    rest.post('http://localhost:8000/api/users', (req, res, ctx) => {
+    rest.post("http://localhost:8000/api/users", (req, res, ctx) => {
       return res(
         ctx.json({
-          message: 'Success!'
+          message: "Success!",
         })
-      )
+      );
     })
-  )
-}
+  );
+};
